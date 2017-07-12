2d739f4f0cbf31a1125c491f7a6a9e66986eb272
Antonio-Ordine <ordine@na.infn.it>
2017-07-12 21:02:52 +0200

    Add files via upload

---
+++ gethostbyname.c
@@ -0,0 +1,26 @@
+ 
+#include <netdb.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv) {
+    if (argc < 2) {
+        printf("Usage: %s hostname", argv[0]);
+        exit(-1);
+    }
+
+    struct hostent *hp;
+    hp = gethostbyname(argv[1]);
+
+    if (hp == NULL) {
+       printf("gethostbyname() failed\n");
+    } else {
+       printf("%s = ", hp->h_name);
+       unsigned int i=0;
+       while ( hp -> h_addr_list[i] != NULL) {
+          printf( "n°%d %s ",i, inet_ntoa( *( struct in_addr*)( hp -> h_addr_list[i])));
+          i++;
+       }
+       printf("\n");
+    }
+}
